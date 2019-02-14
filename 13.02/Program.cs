345949efe4af83aba43b25b3c8ea6e6144399426
ОбидинК <@205_9>
2019-02-14 16:36:07 +0600

    asd

--- 13.02/Program.cs
+++ 13.02/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using _13._02.NewFolder1;
+using Newtonsoft.Json;
+
 namespace _13._02
 {
     class Program
@@ -11,7 +13,10 @@ namespace _13._02
         static void Main(string[] args)
         {
             serviceUser asd = new serviceUser("https://randomuser.me/api?results=1");
-            Console.WriteLine(asd.GetJsonUser());
+            //Console.WriteLine(asd.GetJsonUser());
+            string json = asd.GetJsonUser();
+            var users = JsonConvert.DeserializeObject<Users>(json);
+            
         }
     }
 }
