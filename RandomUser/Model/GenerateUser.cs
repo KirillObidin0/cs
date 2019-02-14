e0b145c12514e92e0eae7e816408268fe4bb15d5
ОбидинК <@205_9>
2019-02-14 18:10:06 +0600

    -

---
+++ RandomUser/Model/GenerateUser.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomUser.Model
+{
+   public  class GenerateUser
+    {
+        public static results GetUser()
+        {
+            serviceUser asd = new serviceUser("https://randomuser.me/api?results=1");
+            //Console.WriteLine(asd.GetJsonUser());
+            string json = asd.GetJsonUser();
+            var users = JsonConvert.DeserializeObject<Users>(json);
+            return users.results[0];
+        }
+    }
+}
